e9c488566d2968a68a74e3a4a066ea65765dcad3
Pauline Rudge <paulinerudge@me.com>
2019-01-18 14:26:06 +0000

    Test look for a pet that isn't there and return nil

--- pet_shop.rb
+++ pet_shop.rb
@@ -23,26 +23,24 @@ def stock_count(pet_shop)
   return pet_shop[:pets].length
 end
 
-# def pets_by_breed(pet_shop, pet_breed)
-#   # total number of a certain breed
-#   pets = []
-#   breed_total = 0
-#   for pet in pet_shop[:pets]
-#     if pet[:breed] == pet_breed
-#       breed_total += 1
-#     end
-#   end
-#   return breed_total
-# end
-
 def pets_by_breed(pet_shop, pet_breed)
   # total number of a certain breed
   pets = []
-  
+# go thru each pet in the pets in pet_shop array
   for pet in pet_shop[:pets]
     if pet[:breed] == pet_breed
       pets << pet[:name]
     end
   end
   return pets
+end
+
+def find_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      return pet
+    end
+  end
+  # no pets found so return nil
+  return nil
 end
