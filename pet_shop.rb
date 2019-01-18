71199daea45a286d7d2babedc8956275dcee10ee
Pauline Rudge <paulinerudge@me.com>
2019-01-18 14:05:34 +0000

    Test that a certain breed of pets outputs correct number of items in array

--- pet_shop.rb
+++ pet_shop.rb
@@ -22,3 +22,27 @@ def stock_count(pet_shop)
   # stock count is number of items in pets array
   return pet_shop[:pets].length
 end
+
+# def pets_by_breed(pet_shop, pet_breed)
+#   # total number of a certain breed
+#   pets = []
+#   breed_total = 0
+#   for pet in pet_shop[:pets]
+#     if pet[:breed] == pet_breed
+#       breed_total += 1
+#     end
+#   end
+#   return breed_total
+# end
+
+def pets_by_breed(pet_shop, pet_breed)
+  # total number of a certain breed
+  pets = []
+  breed_total = 0
+  for pet in pet_shop[:pets]
+    if pet[:breed] == pet_breed
+      pets << pet[:name]
+    end
+  end
+  return pets
+end
