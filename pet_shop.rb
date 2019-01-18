ea7e2fd53a7e3dc3d04cffeb0a0b12e605ac4d71
Pauline Rudge <paulinerudge@me.com>
2019-01-18 16:07:29 +0000

    sell a pet to a customer and checked customer pet count, no of pets sold, customer_cash and total_cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -71,3 +71,26 @@ def customer_can_afford_pet(customer, new_pet_hash)
   return true if customer[:cash] >= new_pet_hash[:price]
   return false
 end
+
+def sell_pet_to_customer(pet_shop, new_pet, customer)
+
+  if customer_can_afford_pet(customer, new_pet)
+    # add pet to customer,
+    # remove from seller
+    # take funds from customer
+    # add them to seller
+
+    # take funds from customer
+    #customer[:cash] -= new_pet_hash[:price]
+
+    # add pet hash to customer,
+    customer[:pets] << new_pet
+    # add pet to pets sold
+    pet_shop[:admin][:pets_sold] += 1
+    p pet_shop[:admin]
+    # take funds from customer
+    customer[:cash] -= new_pet[:price]
+    # add the price of the new sell to the total sell
+    pet_shop[:admin][:total_cash] += new_pet[:price]
+  end
+end
